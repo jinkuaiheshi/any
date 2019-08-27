efec4605ae09af725881c4766bc3c38600af57fe
jinkuaiheshi <jinkuaiheshi@163.com>
2019-08-27 15:57:02 +0800

    echo

--- app/Http/Controllers/Admin/SmokeController.php
+++ app/Http/Controllers/Admin/SmokeController.php
@@ -403,12 +403,7 @@ class SmokeController extends CommonController
     }
     public function tick(){
         $raw_input = file_get_contents('php://input');
-      //  $resolved_body = Util::resolveBody($raw_input);
-
-        if (is_array($resolved_body)){
-            $msg = 'Array|' . json_encode($resolved_body);
-        }
-
-        file_put_contents('/tmp/util_test', $msg . "\n", FILE_APPEND);
+        $resolved_body = Util::resolveBody($raw_input);
+        echo $resolved_body;
     }
 }
